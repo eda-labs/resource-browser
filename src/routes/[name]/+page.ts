89f187b8eb7b4b3fd696ba9bba1b9accd0297681
Siva Sivakumar <siva.sivakumar@nokia.com>
2025-07-17 16:24:16 +0200

    local storage

--- src/routes/[name]/+page.ts
+++ src/routes/[name]/+page.ts
@@ -8,31 +8,35 @@ const resources = crdResources as CrdVersionsMap
 export async function load({ fetch, params }) {
   const [name, versionOnFocus] = params.name.split("_")
 
-  if(!Object.keys(resources).includes(name)) {
-    throw error(404, "Invalid resource name")
-  }
+  if(name.startsWith("uploaded-")) {
+    return { name, versionOnFocus, uploaded: true }
+  } else {
+    if(!Object.keys(resources).includes(name)) {
+      throw error(404, "Invalid resource name")
+    }
 
-  if(!resources[name].includes(versionOnFocus)) {
-    throw error(404, "Invalid version for the resource name")
-  }
+    if(!resources[name].includes(versionOnFocus)) {
+      throw error(404, "Invalid version for the resource name")
+    }
 
-  try {
-    const resp = await fetch(`/resources/${name}/resource.json`)
-    const crd = await resp.json()
+    try {
+      const resp = await fetch(`/resources/${name}/resource.json`)
+      const crd = await resp.json()
 
-    const group = crd.spec.group
-    const kind = crd.spec.names.kind
-    const versions: VersionSchema = {}
+      const group = crd.spec.group
+      const kind = crd.spec.names.kind
+      const versions: VersionSchema = {}
 
-    crd.spec.versions.forEach((x: OpenAPISchema) => {
-      versions[x.name] = {
-        spec: x.schema.openAPIV3Schema.properties.spec,
-        status: x.schema.openAPIV3Schema.properties.status
-      }
-    })
+      crd.spec.versions.forEach((x: OpenAPISchema) => {
+        versions[x.name] = {
+          spec: x.schema.openAPIV3Schema.properties.spec,
+          status: x.schema.openAPIV3Schema.properties.status
+        }
+      })
 
-    return { name, group, kind, versions, versionOnFocus }
-  } catch(e) {
-    throw error(404, "Error fetching resource" + e)
+      return { name, group, kind, versions, versionOnFocus, uploaded: false }
+    } catch(e) {
+      throw error(404, "Error fetching resource" + e)
+    }
   }
 }
